4c9645311e82adda3c58b193c1d84c7e69ce75fe
Мария Гридина <9_chasov@mail.ru>
2022-07-05 20:36:50 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance0 = 2000; //баланс начальный
+        int refill = 300; //бонусное пополнение
+        int x = 100; //шаг бонуса
+        int bonus = refill / x; //бонусные рубли
+
+        if (refill > 1000) {
+            System.out.println("Итоговый счет: " + (bonus + balance0 + refill) + " руб");
+        } else {
+            System.out.println("Итоговый счет: " + (balance0 + refill) + " руб");
+            System.out.println("Для получения бонусов пополните счет на сумму более 1000 рублей. Хорошего дня)");
+        }
+
+        if (refill > 1000) {
+            System.out.println("Количество бонусных рублей: " + bonus);
+
+        }
+    }
+
+}
